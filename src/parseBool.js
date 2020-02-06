34bd43fb9d17eb86e20a13adc9c86f226000e3bd
Sven <sven.fehler@web.de>
2020-02-06 09:19:40 +0100

    more stuff

--- src/parseBool.js
+++ src/parseBool.js
@@ -2,20 +2,22 @@
 /**
  * Parses an input and returns its boolean equivalent
  * @param {*} input
- * @returns {Boolean}
+ * @returns {Boolean|undefined} Returns undefined if variable couldn't be parsed
  */
 const parseBool = input => {
     if(input === "true"
     || input === "1"
     || input === "yes"
     || input === "y"
-    || input === 1) return true;
+    || input === 1
+    || input == true) return true;
     else
     if(input === "false"
     || input === "0"
     || input === "no"
     || input === "n"
-    || input === 0) return false;
+    || input === 0
+    || input == false) return false;
     else return undefined;
 }
 
